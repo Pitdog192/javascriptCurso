2a4f61c3ba2c40c5cdaf320a35d20bc5ca3e676e
Santiago Arroquigaray <arroqui192@gmail.com>
2022-06-03 15:52:41 -0300

    Eventos parte 1

--- javascript/js.js
+++ javascript/js.js
@@ -6,35 +6,72 @@ class productos {
         this.stock=parseInt(stock);
     }
 }
-const prodArr = []; //Array de productos
-const agrega = function(index){
-    prodArr.push(new productos(index+1, prompt("agrega nombre"), parseFloat(prompt("agrega precio")), parseInt(prompt("Cantidad de stock disponible"))))
-}
-
-let cantProd= parseInt(prompt("Sos el vendedor, definí la antidad de productos para agregar a la venta"));
-
-for (let index = 0; index < cantProd; index++) {
-    agrega(index)
-}
-
-console.log(prodArr);
 
 const tablaBody =  document.getElementById("tablaBody");
+const nombre = document.getElementById('nombre');
+const precio = document.getElementById('precio');
+const stock = document.getElementById('stock');
+const btnsubmit = document.getElementById('btnsubmit');
+const formulario = document.getElementById('formulario');
+const leyenda1 = document.getElementById('leyenda1');
+const leyenda2 = document.getElementById('leyenda2');
+const leyenda3 = document.getElementById('leyenda3');
+let vali1 = document.createElement("small");
+let vali2 = document.createElement("small");
+let vali3 = document.createElement("small");
+const prodArr = []; //Array de productos
+let click=0;
 
-for(const productos of prodArr){
-    let fila = document.createElement("tr");
-    fila.innerHTML = `<td> ${productos.id} </td>
-                      <td> ${productos.nombre} </td>
-                      <td> $${productos.precio} </td>
-                      <td> ${productos.stock} </td>
-                      <td><button class="btn btn-primary">Agregar</button></td>`
-    tablaBody.append(fila);                  
-}
+formulario.addEventListener("submit", e=>{
+    e.preventDefault();
+    if(!isNaN(nombre.value) || nombre.value == ""){
+        vali1.className = "form-text text-danger";
+        vali1.innerHTML = `Tiene que ingresar un nombre valido`;
+        nombre.style.border ="3px solid red";
+        leyenda1.append(vali1);
+        return;
+    } 
+    if(precio.value == ""){
+        vali2.className = "form-text text-danger";
+        vali2.innerHTML = `Tiene que ingresar un precio`;
+        precio.style.border ="3px solid red";
+        leyenda2.append(vali2);
+        return;
+    } 
+    if(stock.value == ""){
+        vali3.className = "form-text text-danger";
+        vali3.innerHTML = `Tiene que ingresar stock`;
+        stock.style.border ="3px solid red";
+        leyenda3.append(vali3);
+        return;
+    } 
+    btnsubmit.onclick = () => {
+        prodArr.push(new productos(click+1, nombre.value , precio.value , stock.value));
+        click+=1;
+        let fila = document.createElement("tr");
+        fila.innerHTML = `<td> ${click} </td>
+                          <td> ${nombre.value} </td>
+                          <td> $${precio.value} </td>
+                          <td> ${stock.value} </td>
+                          <td><button class="btn btn-primary">Agregar al carrito</button></td>`
+        tablaBody.append(fila);
+        nombre.style.border ="3px solid white";                  
+        precio.style.border ="3px solid white";
+        stock.style.border ="3px solid white";
+        if(leyenda1.append(vali1)){
+            leyenda1.removeChild(vali1);
+        } else if (leyenda2.append(vali2)){
+            leyenda2.removeChild(vali2);
+        } else if (leyenda3.append(vali3)){
+            leyenda3.removeChild(vali3);
+        }
+    }
+});
+
+btnsubmit.onmouseover = () => {btnsubmit.className = "btn btn-success";}
+btnsubmit.onmouseout = () => {btnsubmit.className = "btn btn-primary";}
+
+
 
 //Formato de la tabla
-tabla.className="table table-striped table-dark text-center"
-if (prodArr.length <= 2){
-    tabla.className="table table-striped table-primary text-center"
-} else {
-    tabla.className="table table-striped table-dark text-center"
-}
+tabla.className="table table-striped table-dark text-center";
