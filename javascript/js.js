2232dc672562e05b633ec86e23e9f04a4a1b44fe
Santiago Arroquigaray <arroqui192@gmail.com>
2022-06-06 13:05:57 -0300

    Eventos

--- javascript/js.js
+++ javascript/js.js
@@ -1,3 +1,5 @@
+window.addEventListener('load', ()=> {
+
 class productos {
     constructor(id,nombre,precio,stock){
         this.id=parseInt(id);
@@ -7,71 +9,71 @@ class productos {
     }
 }
 
-const tablaBody =  document.getElementById("tablaBody");
 const nombre = document.getElementById('nombre');
 const precio = document.getElementById('precio');
 const stock = document.getElementById('stock');
 const btnsubmit = document.getElementById('btnsubmit');
 const formulario = document.getElementById('formulario');
-const leyenda1 = document.getElementById('leyenda1');
-const leyenda2 = document.getElementById('leyenda2');
-const leyenda3 = document.getElementById('leyenda3');
-let vali1 = document.createElement("small");
-let vali2 = document.createElement("small");
-let vali3 = document.createElement("small");
+const tablaBody = document.getElementById('tablaBody');
 const prodArr = []; //Array de productos
 let click=0;
 
 formulario.addEventListener("submit", e=>{
     e.preventDefault();
-    if(!isNaN(nombre.value) || nombre.value == ""){
-        vali1.className = "form-text text-danger";
-        vali1.innerHTML = `Tiene que ingresar un nombre valido`;
-        nombre.style.border ="3px solid red";
-        leyenda1.append(vali1);
-        return;
-    } 
-    if(precio.value == ""){
-        vali2.className = "form-text text-danger";
-        vali2.innerHTML = `Tiene que ingresar un precio`;
-        precio.style.border ="3px solid red";
-        leyenda2.append(vali2);
-        return;
-    } 
-    if(stock.value == ""){
-        vali3.className = "form-text text-danger";
-        vali3.innerHTML = `Tiene que ingresar stock`;
-        stock.style.border ="3px solid red";
-        leyenda3.append(vali3);
-        return;
-    } 
-    btnsubmit.onclick = () => {
-        prodArr.push(new productos(click+1, nombre.value , precio.value , stock.value));
-        click+=1;
-        let fila = document.createElement("tr");
-        fila.innerHTML = `<td> ${click} </td>
-                          <td> ${nombre.value} </td>
-                          <td> $${precio.value} </td>
-                          <td> ${stock.value} </td>
-                          <td><button class="btn btn-primary">Agregar al carrito</button></td>`
-        tablaBody.append(fila);
-        nombre.style.border ="3px solid white";                  
-        precio.style.border ="3px solid white";
-        stock.style.border ="3px solid white";
-        if(leyenda1.append(vali1)){
-            leyenda1.removeChild(vali1);
-        } else if (leyenda2.append(vali2)){
-            leyenda2.removeChild(vali2);
-        } else if (leyenda3.append(vali3)){
-            leyenda3.removeChild(vali3);
-        }
-    }
+    validarInput();
 });
-
-btnsubmit.onmouseover = () => {btnsubmit.className = "btn btn-success";}
-btnsubmit.onmouseout = () => {btnsubmit.className = "btn btn-primary";}
-
-
 
 //Formato de la tabla
 tabla.className="table table-striped table-dark text-center";
+
+const validacionMal = (input, mensaje) => {
+    const divForm = input.parentElement;
+    const texto = divForm.querySelector('p');
+    texto.innerText = mensaje;
+    texto.style.color= 'red';
+    input.style.border ="3px solid red";
+}
+
+const validacionBien = (input,mensaje) => {
+    const divForm = input.parentElement;
+    const texto = divForm.querySelector('p');
+    texto.innerText = mensaje;
+    texto.style.color= 'green';
+    input.style.border ="3px solid green";
+    if (nombre.value && isNaN(nombre.value) && precio.value && stock.value){
+        btnsubmit.onclick = () => {
+            prodArr.push(new productos(click, nombre.value , precio.value , stock.value));
+            click+=1;
+            let fila = document.createElement("tr");
+            fila.innerHTML = `<td> ${click} </td>
+                              <td> ${nombre.value} </td>
+                              <td> $${precio.value} </td>
+                              <td> ${stock.value} </td>
+                              <td><button class="btn btn-danger btnBorrar">Borrar</button></td>`
+            tablaBody.append(fila);
+        }
+    } else {
+        btnsubmit.onclick = () => {
+            console.log("ingresa datos");
+        }
+    }
+}
+
+const validarInput = () => {
+    const nombreValor = nombre.value.trim();
+    const precioValor = precio.value.trim();
+    const stockValor = stock.value.trim();
+
+    if (!nombreValor || nombreValor === ''){
+        validacionMal(nombre, 'Campo vacio');
+    }else if (!isNaN(nombreValor)){
+        validacionMal(nombre, 'ingresó un número');
+    }else {
+        validacionBien(nombre, 'Todo correcto');
+    } 
+
+    (!precioValor || precioValor === '') ? validacionMal(precio, 'Campo vacio') : validacionBien(precio, 'Todo correcto');
+    (!stockValor || stockValor === '') ? validacionMal(stock, 'Campo vacio') : validacionBien(stock, 'Todo correcto');
+}
+})
+
